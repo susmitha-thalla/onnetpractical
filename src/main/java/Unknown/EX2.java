2814ab93f27929317661016ca46e2d64ad9f920f
susmitha <thallasusmitha548@gmail.com>
2026-08-11 12:35:58 +0530

    hit

--- src/main/java/Unknown/EX2.java
+++ src/main/java/Unknown/EX2.java
@@ -3,5 +3,6 @@ package Unknown;
 public class EX2 {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("onnetsystems");
     }
 }
